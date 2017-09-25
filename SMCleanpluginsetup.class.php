94452feb626604f60c9b1c8ab99639512cd4aad8
sMorten <spencer.mortenson@gmail.com>
2017-09-25 17:39:41 -0600

    Updated admin page template

--- SMCleanpluginsetup.class.php
+++ SMCleanpluginsetup.class.php
@@ -7,12 +7,20 @@ function __construct(){
 	$this->setOptions();
 
 
-}	
+}
+
+
 
 //Load admin functions
 function adminInit(){
 	//load admin pages 
+	add_action('admin_menu',array(this,'sm_clean_page_menu'));
 
+}
+
+//function to add menu page
+function sm_clean_page_menu(){
+	add_menu_page( 'SMWeb Clean Sales Page', 'SMWeb Clean Sales Page', 'manage_options', 'admin/php/SMCleanPageMenu.php', 'SMCleanPage_admin_page', 'admin/SMWDIconOnly.svg', 6  );
 
 }
 
---
+++ admin/php/SMCleanPageMenu.php
@@ -0,0 +1,23 @@
+<?php
+	function SMCleanPage_admin_page(){
+	?>
+
+
+	<div class="wrap">
+		<img src="SMWD.png" style="width:25%;" />
+		<h2>Welcome to the SM Web Development Clean Page Plugin</h2>
+		<p>This plugin allows for the removal of the navigation, footer, anything really! </p>
+		<ol>
+			<li>Grab the CSS selector for the element you want to hide using the chrome inspector (or whatever you prefer to use)</li>
+			<li>Enter it into the list below</li>
+			<li>Go to the post or page you want to hide it on and click on the Make Clean setting</li>
+			<li>Preview your page</li>
+		</ol>
+	</div>
+
+
+	<?php
+}
+
+
+?>
